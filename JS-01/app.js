8421b93625ec762766ce07a03540cf7f5e967223
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2023-12-07 20:07:25 +0600

    Код 1го урока

---
+++ JS-01/app.js
@@ -0,0 +1,66 @@
+// console.log(100 + 100);
+
+// console.log(20, "hello", 350);
+
+// console.warn("Абайла!");
+
+// console.error("Ошибка в коде");
+
+// console.table({
+//   name: "John",
+//   email: "john@john.com",
+// });
+
+// ;aklsdjf;aksdj
+// comment
+// comment 2
+
+/*
+kasjdf
+;lkajdsf
+;aksdf
+;kasdjf
+*/
+
+// console.log()
+
+/* console.log()
+console.log() */
+
+//  CTRL + /
+// ALT + SHIFT + A
+
+// RAM
+
+// Переменные - variable
+
+// var user;
+// user = "Masha";
+
+// console.log(user);
+// alert(user);
+
+// let user = "Masha";
+// console.log(user);
+
+// user = "Pete";
+// console.log(user);
+
+// user = 550;
+// console.log(user);
+
+const birth = 2000;
+console.log(birth);
+
+let userName = "Masha";
+
+let $ = "adsdfasdfasd";
+
+// birth = 2005;
+// console.log(birth);
+
+// Начинается с маленькой буквы (латиница, английские слова)
+// использовать camelCase
+// не начинается с цифры
+// $
+// не использовать зарезервированные слова
