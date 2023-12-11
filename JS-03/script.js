3cdb59e2f7440218eabc5796414b8a8dcdf3a0c8
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2023-12-11 21:57:31 +0600

    Код 03-урока

---
+++ JS-03/script.js
@@ -0,0 +1,66 @@
+// let userName = "Hello";
+
+// // let userNameLength = userName.length;
+
+// console.log(userName[0]);
+
+// конкатенация строк
+// let firstName = "John";
+// let lastName = "Doe";
+// let fullName = firstName + " " + lastName;
+
+// console.log(`Hello, ${fullName}`);
+
+// let s = "hello world, i am cool programmer";
+// console.log(s.__proto__);
+// console.log(s.toUpperCase());
+
+// let sInLowerCase = s.toLocaleLowerCase();
+// let sInUpperCase = s.toUpperCase();
+
+// console.log(s.charAt(2));
+// console.log(s.indexOf("A"));
+
+// подстрока, срезы
+// console.log(s.substring(2, 6));
+// console.log(s.slice(-2));
+
+// let userName = "tASHbolOT";
+// let firstLetter = userName[0].toLocaleUpperCase();
+// let x = userName.slice(1).toLocaleLowerCase();
+
+// let formattedUserName = firstLetter + x;
+
+// console.log(formattedUserName);
+
+// let userName = "tashbolot";
+// // console.log(userName);
+// // console.log(userName.trimStart());
+// // console.log(userName.trimEnd());
+// // console.log(userName.trim());
+// // console.log(userName.replace("o", "123"));
+// console.log(userName.repeat(20));
+
+// let numb = 5.45461321;
+
+// console.log(numb.toString());
+// console.log(numb.toFixed(3));
+// console.log(numb.toPrecision(2));
+// console.log(numb.toLocaleString("ar-EG"));
+
+// let x = 20;
+
+// let a = 34.1546546;
+
+// console.log(Math.ceil(a));
+// console.log(Math.floor(a));
+// console.log(Math.round(a));
+
+// console.log(Math.min(45, 32, 2, -1, 99));
+// console.log(Math.max(45, 32, 2, -1, 99));
+
+// console.log(Math.floor(Math.random() * 10));
+// console.log(Math.floor(Math.random() * 5 + 1));
+
+let b = "50.12";
+console.log(+b);
