6ed4566aeca6b33de4e9e12320d396058075d4d0
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2023-12-18 20:05:30 +0600

    Add lesson 04 code

---
+++ JS-04/script.js
@@ -0,0 +1,75 @@
+// Data
+
+// let d = new Date(2001, 5, 10, 12, 34, 0);
+// let d = new Date("2001-05-10T12:34:00");
+
+// unix timestamps 1 january 1970
+// let now = Date.now();
+
+// let myDay = new Date(2001, 5, 10, 12, 34, 0);
+// console.log(myDay);
+// console.log(myDay.getTime());
+// console.log(myDay.getFullYear());
+// console.log(myDay.getDay());
+// console.log(myDay.getDate());
+
+// Intl
+// let myDay = new Date(2001, 5, 10, 12, 34, 0);
+
+// let formatter = new Intl.DateTimeFormat("de", {
+//   weekday: "long",
+//   year: "numeric",
+//   month: "long",
+//   day: "numeric",
+// });
+
+// console.log(formatter.format(myDay));
+
+// array
+// let friends = ["masha", "pete", "tashmat"];
+
+// constructor
+// let friends = new Array("masha", "pete", "vasya");
+// friends[0] = "john";
+// friends[99] = "tashmat";
+// console.log(friends.length);
+
+const vegetables = ["tomato", "onion"];
+
+// const fruitsAndVegs = fruits + vegetables;
+
+// const fruits = ["grape", "masha", "pete", "masha", "banana", "lemon"];
+// console.log(fruits.sort());
+// fruits.push("banana");
+// fruits.push("apple");
+// fruits.unshift("grape");
+
+// const lastDeleted = fruits.pop();
+// fruits.shift();
+// fruits.shift();
+// fruits.shift();
+// fruits.shift();
+
+// console.log(fruits.reverse());
+// console.log(fruits.includes("masha"));
+// console.log(fruits.indexOf("masha"));
+// console.log(fruits.slice(0, 3).reverse());
+// console.log(fruits[0].toUpperCase());
+
+const numbers = [10, 2, 30, 1, 45, 2, 31];
+// console.log(numbers.sort());
+// const numbers2 = [4, 5, 6];
+
+// // spread
+// const numbers3 = [...numbers, ...numbers2];
+// console.log(numbers3);
+
+// foreach
+// map
+// filter
+// reduce
+
+const animals = ["Dodo", "Tiger", "Penguin", "Dodo0"];
+
+console.log(animals.lastIndexOf("Dodo"));
+// Expected output: 3
