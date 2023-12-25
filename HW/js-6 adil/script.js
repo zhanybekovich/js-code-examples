571f12bfebf966a5932d50bed3a99374da217e99
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2023-12-25 20:16:41 +0600

    Add Adil HW

---
+++ HW/js-6 adil/script.js
@@ -0,0 +1,40 @@
+// Задания №=1
+
+// const num = prompt("ведите число")
+// const n = +num
+// n % 2 == 0 ? alert("ваше число четное"): alert("ваше число не четное");
+
+// Задания №=2
+
+const num = 13;
+
+// if (typeof num === "string") {
+//   console.log("string");
+// }
+
+// num = "" ?  console.log("strings"): console.log("num");
+
+// Задания №=3
+
+// for (let i = 2; i <= 10; i++) {
+//   for (let j = 1; j <= 10; j++) {
+//     console.log(`${i} * ${j} = ${i * j}`);
+//   }
+// }
+
+// Задания №=4
+
+// const user = {
+//   name: "Иван",
+//   age: 25,
+//   email: "ivan@example.com",
+//   city: "Москва",
+// };
+
+// for (const key in user) {
+//   console.log(key, "--->", user[key]);
+// }
+
+// for (key of Object.keys(user)) {
+//   console.log(user[key]);
+// }
