cae50cf21c8d6e4296c6d37d795cb9320fe03696
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-01-04 21:15:59 +0600

    Add lesson DOM-1

---
+++ JS-DOM/DOM-1/script.js
@@ -0,0 +1,24 @@
+// console.log(document);
+
+// console.log(document.head);
+// console.log(document.body.innerHTML);
+
+// console.log(document.body.innerText);
+
+// console.log(document.links[1]);
+
+// document.body.innerHTML = "<h1>Hello</h1>";
+// document.body.innerHTML += "<h2>Hello2</h2>";
+
+// document.write("<h1>Salam!</h1>");
+// document.write("<h2>Salam2!</h2>");
+
+// console.log(document.head.children);
+// console.log(document.body.children);
+// console.log(document.doctype);
+// console.log(document.domain);
+// console.log(document.URL);
+
+// console.log(document.forms[0].id);
+// console.log(document.forms[0].method);
+console.log(document.forms[0].action);
---
+++ JS-DOM/DOM-2/script.js
@@ -0,0 +1,34 @@
+// Single element
+
+// const title = document.getElementById("app-title");
+
+// // изменение классов
+// title.className = "hello title";
+
+// // изменение текста
+// title.textContent = "Hello";
+
+// // изменение стилей
+// title.style.color = "blue";
+// title.style.backgroundColor = "green";
+// title.style.fontSize = "80px";
+
+// // изменение внутреннего HTML
+// title.innerHTML = '<span class="hello">Hello 2</span>';
+
+// const title = document.querySelector("h1");
+// const title = document.querySelector(".title-2");
+// const title = document.querySelector("#app-title");
+
+// const header = document.querySelector("header");
+// const h1 = header.querySelector("h1");
+
+const items = document.querySelectorAll("li");
+
+const buttons = document.querySelectorAll(".remove-item");
+
+// items.forEach((item) => {
+//   item.textContent = "Hello";
+// });
+
+console.log(buttons);
