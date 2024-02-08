0a2b7ce737ffaac9d35c7c90b7ac4c7f61cf9398
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-02-08 21:22:49 +0600

    Lesson 20

---
+++ JS-20/flixx-app-project/js/script.js
@@ -0,0 +1,93 @@
+const global = {
+  currentPage: window.location.pathname,
+};
+
+// Init APP
+function init() {
+  switch (global.currentPage) {
+    case "/":
+    case "/index.html":
+      displayPopularMovies();
+      break;
+    case "/shows.html":
+      console.log("TV Show");
+      break;
+    case "/movie-details.html":
+      console.log("Movie detail");
+      break;
+    case "/tv-details.html":
+      console.log("TV details");
+      break;
+    case "/search.html":
+      console.log("Search");
+      break;
+  }
+
+  highlightActiveLink();
+}
+
+// Highlight Active link
+function highlightActiveLink() {
+  const links = document.querySelectorAll(".nav-link");
+
+  links.forEach((link) => {
+    if (link.getAttribute("href") === global.currentPage) {
+      link.classList.add("active");
+    }
+  });
+}
+
+// FETCH data from API
+async function fetchAPIData(endpoint) {
+  const API_KEY = "0df4d8122ee146e4718616c0bfd1b410";
+  const API_URL = "https://api.themoviedb.org/3/";
+
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+
+  const data = await response.json();
+
+  return data;
+}
+
+// Display popular movies
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData("movie/popular");
+
+  // console.log(results);
+
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+
+    div.innerHTML = `
+    <a href="movie-details.html?id=${movie.id}">
+      ${
+        movie.poster_path
+          ? `<img
+            src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+            class="card-img-top"
+            alt="${movie.title}"
+          />`
+          : `<img
+            src="images/no-image.jpg"
+            class="card-img-top"
+            alt="${movie.title}"
+          />`
+      }
+
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+          </div>
+    `;
+
+    document.querySelector("#popular-movies").appendChild(div);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", init);
